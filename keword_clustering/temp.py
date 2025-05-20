988e25c163f0a7b4129fe77acbcad52a13f39808
sori <alsldjsthflone@daum.net>
2025-05-20 17:03:55 +0900

    22대 클러스터링 불용어 수정

--- keword_clustering/temp.py
+++ keword_clustering/temp.py
@@ -1,3 +1,5 @@
+# kiwi_analyze.py에 불용어/고유명사 추가 위해, 여기서 df 실행해서 샘플 확인
+
 import pandas as pd
 import os 
 
@@ -5,7 +7,7 @@ import os
 pd.set_option('display.max_colwidth', None)
 
 base_dir = 'C:/Users/1-08/OneDrive/Desktop/DAMF2/Final_PJT/'
-file_path = os.path.join(base_dir, 'keword_clustering/data/clustered_bills.csv')
+file_path = os.path.join(base_dir, 'keword_clustering/data/clustered_bills_22.csv')
 df = pd.read_csv(file_path)
 
 df = df[['BILL_NAME', 'BILL_NO', 'cleaned', 'cluster']]
